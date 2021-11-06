de5c23da06aeff998ae72d650deca1ac0b714a03
Islam Ahmadien <islammohamed323@gmail.com>
2021-11-06 16:46:20 +0200

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        DFS dfs = new DFS("125340678","012345678");
-        BFS bfs = new BFS("125340678","012345678");
-        System.out.println(bfs.bfs());
-    }
-}
