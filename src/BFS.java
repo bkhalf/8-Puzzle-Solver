8fe20b1bc8bc88a196995095d8629beb7f0a6e97
bkhalf <bahaakhalf8@gmail.com>
2021-11-06 02:12:43 +0200

    A_STAR

--- src/BFS.java
+++ src/BFS.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
+import java.util.PriorityQueue;
 
 public class BFS {
     private String initial;
@@ -30,7 +33,9 @@ public class BFS {
         }
         return false;
     }
-    private ArrayList<String> getNeighbor(String state){
+    
+    
+private ArrayList<String> getNeighbor(String state){
         ArrayList<String> adj = new ArrayList<>();
         int index = 0,stateLength = state.length();
         for(int i =0;i<stateLength;i++){
@@ -90,5 +95,124 @@ public class BFS {
         }
         return adj ;
     }
+	
+	public Entry A_StarManhattanDistance() {
+		Set<String> explored = new LinkedHashSet<String>();  
+		Entry ans = a_star(explored,true);
+		System.out.println("The explored states");
+		for (String element : explored) {
+			printState(element);
+		}
+		return ans;
+	}
+	
+	public Entry A_StarEuclideanDistance() {
+		Set<String> explored = new LinkedHashSet<String>();  
+		Entry ans = a_star(explored,false);
+		System.out.println("The explored states :");
+		for (String element : explored) {
+			printState(element);
+		}
+		return ans;
+	}
+	
+	public Entry a_star(Set<String> explored,boolean MD) {
+		PriorityQueue<Entry> frontier = new PriorityQueue<>();
+	
+		frontier.add(new Entry(0,calcH(this.initial,this.goal,MD),this.initial,""));
+		while(!frontier.isEmpty()) {
+			Entry state = frontier.poll();
+			explored.add(state.value);
+			
+			if(state.value.equals(this.goal)) {
+				state.path=state.path+"-"+state.value;
+				return state;
+			}
+			
+			ArrayList<String> neighbors = getNeighbor(state.value);
+			
+			for (String n : neighbors) {
+				Entry temp=null;
+				for (Entry element : frontier) {
+					if(n.equals(element.value)) {
+						temp=element;
+						break;
+					}
+				}
+				
+				if(temp!=null) {
+					if(state.g+1<temp.g) {
+						temp.g=state.g+1;
+						frontier.remove(temp);
+						temp.updateKey();
+						temp.path=state.path+"-"+state.value;
+						frontier.add(temp);
+					}
+				}else if(!explored.contains(n)) {
+					frontier.add(new Entry(state.g+1,calcH(n,this.goal,MD),n,state.path+"-"+state.value));
+				}
+				
+			}
+			
+		}
+		
+		return null;
+	}
+	// calculate the heuristic function 
+	public float calcH(String state,String goal,boolean MD) {
+		float ans = 0;
+		for(int i=0;i<goal.length();i++) {
+			int j=state.indexOf(goal.charAt(i));
+			int x1,y1,x2,y2;
+			
+			y1=i%3;x1=(i*3)/9;                            //calculate the x and y axes to the goal element
+			y2=j%3;x2=(j*3)/9;						      //calculate the x and y axes to the state element
+			if(MD)
+				ans += Math.abs(x1-x2)+Math.abs(y1-y2);       //calculate the Manhattan Distance
+			else
+				ans += Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));       //calculate the Euclidean Distance
+		}
+		return ans;
+	}
+	public void printState(String s){
+		System.out.println("|"+s.charAt(0)+"|"+s.charAt(1)+"|"+s.charAt(2)+"|");
+		System.out.println("|"+s.charAt(3)+"|"+s.charAt(4)+"|"+s.charAt(5)+"|");
+		System.out.println("|"+s.charAt(6)+"|"+s.charAt(7)+"|"+s.charAt(8)+"|");
+		System.out.println("----------------------");
+	}
 
+}
+class Entry implements Comparable<Entry> {
+    public float key;
+    public int g;
+    public float h;
+    public String value;
+    String path;
+    
+    public Entry(int g,float h, String value,String from) {
+    	this.g=g;
+    	this.h=h;
+        this.key = g+h;
+        this.value = value;
+        this.path=from;
+    }
+    public void updateKey() {
+    	key=g+h;
+    }
+    // getters
+
+    @Override
+    public int compareTo(Entry other) {
+    	if(this.key > other.key){
+            return 1;
+        }
+
+        else if (this.key < other.key){
+            return -1;
+        }
+
+        else{
+            return 0;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        DFS dfs = new DFS("125340678","012345678");
-        BFS bfs = new BFS("125340678","012345678");
-        System.out.println(bfs.bfs());
+//        DFS dfs = new DFS("125340678","012345678");
+//        BFS bfs = new BFS("125340678","012345678");
+    	BFS bfs = new BFS("123046758","123456780");
+        Entry ans=bfs.A_StarEuclideanDistance();
+//        System.out.println(ans.value);
+        System.out.println(ans.key);
+        System.out.println(ans.path);
     }
 }
